36b8c732de7e39775d40ac5bc7243063e9ffecb3
Nupura <mnupura@gmail.com>
2021-06-19 15:29:38 +0530

    Modules

---
+++ Program_Calculator.py
@@ -0,0 +1,14 @@
+import Module_Calculator
+from Module_Calculator import multiply,divide
+
+result=Module_Calculator.add(2,3)
+print("Add: {}".format(result))
+
+result=Module_Calculator.add(12,3)
+print("Subtract: {}".format(result))
+
+result=multiply(2,3)
+print("Multiply: {}".format(result))
+
+result=divide(15,5)
+print("Division: {}".format(result))
