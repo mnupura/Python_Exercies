b7a25d0a745a6cf9389b3aa3c3b908d26039c1f7
Nupura <mnupura@gmail.com>
2021-10-10 23:22:40 +0530

    OOP school proj v3

--- Oops_School_Project.py
+++ Oops_School_Project.py
@@ -5,15 +5,27 @@ class School():
         self.__students = []
 
     def admit_student(self,student):
+        student.assign_roll_number(len(self.__students)+1)
         self.__students.append(student)
-        print(self.__students[0].name)
-        print(self.__students[0].age)
+
+    def display_student(self):
+        for stud in self.__students:
+            print(stud.name)
+            print(stud.age)
+            print(stud.roll_number)
 
 class Student():
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    def assign_roll_number(self, roll_number):
+        self.roll_number = roll_number
+
+
 DES = School()
+
 stud1 = Student('Nupura',10)
 DES.admit_student(stud1)
+
+DES.display_student()
