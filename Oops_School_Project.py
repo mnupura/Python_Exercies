40f844bff58c105e94d1aead1f69f8791087c046
Nupura <mnupura@gmail.com>
2021-12-03 20:30:14 +0530

    Version 5

--- Oops_School_Project.py
+++ Oops_School_Project.py
@@ -1,18 +1,27 @@
 
 class School():
+    admission_limit = 0
+
     def __init__(self):
-        self.__admission_limit = 20
         self.__students = []
 
-    def admit_student(self,student):
-        student.assign_roll_number(len(self.__students)+1)
-        self.__students.append(student)
+    def admit_student(self, student):
+        if self.admission_limit < 3:
+            self.admission_limit+=1
+            student.assign_roll_number(len(self.__students)+1)
+            self.__students.append(student)
+            return True
+        else:
+            print("Admissions full. No more seats available")
+            return False
+                    
 
     def display_student(self):
+        print('------------------------------------------------')
         for stud in self.__students:
-            print(stud.name)
-            print(stud.age)
-            print(stud.roll_number)
+            print('Student Roll Numer: '+str(stud.roll_number))
+            print('Student Name: '+stud.name)
+            print('Student Age: '+str(stud.age))
 
 class Student():
     def __init__(self, name, age):
@@ -24,12 +33,11 @@ class Student():
 
 
 DES = School()
-entry=0
-while entry < 3:
+limit = True
+while limit:
     name = input('Enter name: ')
     age = input('Enter age: ')
     stud = Student(name, age)
-    DES.admit_student(stud)
-    entry+=1
+    limit=DES.admit_student(stud)
 
 DES.display_student()
