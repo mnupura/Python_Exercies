b8b49f20df93583a487dc926ac8ebbb3968824fe
Jai Purandare <jai2005@gmail.com>
2022-01-26 23:52:03 -0800

    Added comments for improvements

--- Oops_School_Project.py
+++ Oops_School_Project.py
@@ -7,7 +7,7 @@ class School():
         self.students = []
 
     def admit_student(self, student):
-        if len(self.students) < School.ADMISSION_LIMIT:
+        if len(self.students) < School.ADMISSION_LIMIT: # replace with if self.is_admission_available()
             student.assign_roll_number(len(self.students)+1)
             self.students.append(student)
             return True
@@ -20,7 +20,9 @@ class School():
 
     def display_student(self):
         print('------------------------------------------------')
-        for stud in self.students:
+        for stud in self.students: 
+            # write __str__ in the Student so that you don't do stud.roll_number and stud.name and stud.age
+            # It should be simply print(stud)
             print('Student Roll Number: '+str(stud.roll_number))
             print('Student Name: '+stud.name)
             print('Student Age: '+str(stud.age))
@@ -28,7 +30,8 @@ class School():
     def calculate_score(self):
         score = 0
         roll = 0
-        for stud in self.students:
+        for stud in self.students: # variable name should not be shortform
+            #change stud to student
             roll = stud.roll_number
             score = ((roll + 50)/(len(self.students)))
             School.result[roll] = [stud.name,score]
@@ -36,7 +39,7 @@ class School():
     def display_result(self):
         print('------------------------------------------------')
         print('Roll Number'+ '  |  '+'Student Name'+'  |  '+'Score')
-        for x,y in School.result.items():
+        for x,y in School.result.items(): # what crap is x and y ????
             print(str(x) + '            |  '+str(y[0])+'            |  '+str(y[1]))
         
 
